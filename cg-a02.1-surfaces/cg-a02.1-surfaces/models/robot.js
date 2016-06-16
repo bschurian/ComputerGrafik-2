7fe5c38ebbc789efe67cec85c6fb0a3a29cb79ed
robert_dimitrov <robert.dimitrov3@gmail.com>
2016-06-16 15:18:05 +0200

    ...

--- cg-a02.1-surfaces/cg-a02.1-surfaces/models/robot.js
+++ cg-a02.1-surfaces/cg-a02.1-surfaces/models/robot.js
@@ -6,18 +6,19 @@ define(["three"],
 
         var Robot = function(){
             
-            var cube = new THREE.CubeGeometry( 40, 40, 40);
-            var cylinder = new THREE.CylinderGeometry( 1, 1, 1, 8);
-            var sphere = new THREE.SphereGeometry( 1, 8, 8);
+            var cube = new THREE.CubeGeometry( 1,1,1 );
+            var cylinder = new THREE.CylinderGeometry( 1, 1, 1, 1);
+            var sphere = new THREE.SphereGeometry( 1, 1, 1);
             
-            var material = new THREE.MeshBasicMaterial( {color: 0x0000aa} );
-            
+            var material = new THREE.MeshPhongMaterial( {color: 0xf02222} );
+            var phong = new THREE.MeshPhongMaterial({color: 0x969595, specular: 0xDED9D9, shininess: 0});
+
             var headSize  = [130, 130, 130];
             var handSize  = [ 15,  15,  15];
             var uArmSize  = [ 25,  80,  25];
             var ellSize   = [ 25,  15,  15];
             var oArmSize  = [ 35, 100,  35];
-            var schulSize = [ 30,  30,  30];
+            var schulSize = [ 15, 15, 15 ];
             var torsoSize = [250, 400, 150];
             // weitere Groessen
 
@@ -45,7 +46,7 @@ define(["three"],
             this.rOArm.name = "rOArm";
             this.rOArm.add(this.rEll);
             this.rOArm.translateY(-(oArmSize[1]/2 - schulSize[1]/2));
-            
+
             this.rSchul = new THREE.Object3D();
             this.rSchul.name = "rSchul";
             this.rSchul.add(this.rOArm);
@@ -57,8 +58,7 @@ define(["three"],
             this.torso.name = "torso";
             this.torso.add(this.head);
             this.torso.add(this.rSchul);
-            
-            
+
             //root
             this.root = new THREE.Object3D();
             this.root.add(this.torso);
@@ -66,26 +66,34 @@ define(["three"],
 
             // skin
             //var cubeMesh = ;
-            this.headSkin = new THREE.Mesh( cube, material);
+            this.headSkin = new THREE.Mesh( cube, phong);
             this.headSkin.rotateY(Math.PI/4);
+            this.headSkin.scale.set(headSize[0], headSize[1], headSize[2]);
             this.head.add(this.headSkin);
             
-            this.rHandSkin = new THREE.Mesh( cube, material);
+            this.rHandSkin = new THREE.Mesh( cube, phong);
+            this.rHandSkin.scale.set(handSize[0], handSize[1], handSize[2]);
             this.rHand.add(this.rHandSkin);
             
-            this.rUArmSkin = new THREE.Mesh( cube, material);
+            this.rUArmSkin = new THREE.Mesh( cube, phong);
+            this.rUArmSkin.scale.set(uArmSize[0], uArmSize[1], uArmSize[2]);
             this.rUArm.add(this.rUArmSkin);
-            
-            this.rEllSkin = new THREE.Mesh( cube, material);
+
+            this.rEllSkin = new THREE.Mesh( cube, phong);
+            this.rEllSkin.scale.set(ellSize[0], ellSize[1], ellSize[2]);
             this.rEll.add(this.rEllSkin);
+
             
-            this.rOArmSkin = new THREE.Mesh( cube, material);
+            this.rOArmSkin = new THREE.Mesh( cube, phong);
+            this.rOArmSkin.scale.set(oArmSize[0], oArmSize[1], oArmSize[2]);
             this.rOArm.add(this.rOArmSkin);
 
-            this.rSchulSkin = new THREE.Mesh( cube, material);
+            this.rSchulSkin = new THREE.Mesh( new THREE.CubeGeometry(1,1,1), phong);
+            this.rSchulSkin.scale.set(schulSize[0], schulSize[1], schulSize[2]);
             this.rSchul.add(this.rSchulSkin);
 
-            this.torsoSkin = new THREE.Mesh( cube, material);
+            this.torsoSkin = new THREE.Mesh( cube, phong);
+            this.torsoSkin.scale.set(torsoSize[0], torsoSize[1], torsoSize[2]);
             this.torso.add(this.torsoSkin);
             
             console.log(this.root);
