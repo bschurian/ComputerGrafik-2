547f58ae4c66c9864ea61c7e4b03043ddc0201ea
robert_dimitrov <robert.dimitrov3@gmail.com>
2016-06-19 11:00:13 +0200

    tangent points

--- cg-a02.1-surfaces/cg-a02.1-surfaces/controller/html_controller.js
+++ cg-a02.1-surfaces/cg-a02.1-surfaces/controller/html_controller.js
@@ -30,6 +30,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
             $("#parametricSurface").hide();
             $("#obj").hide();
             $("#robot").hide();
+            $("#tangent").hide();
 
             $("#btnRandom").click( (function() {
                 $("#random").show();
@@ -40,6 +41,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").hide();
                 $("#obj").hide();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
 
             $("#btnBand").click( (function() {
@@ -51,6 +53,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").hide();
                 $("#obj").hide();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
 
             $("#btnBoxGeometry").click( (function() {
@@ -62,6 +65,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").hide();
                 $("#obj").hide();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
 
             $("#btnSphere").click( (function() {
@@ -73,6 +77,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").hide();
                 $("#obj").hide();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
 
             $("#btnTorusKnot").click( (function() {
@@ -84,6 +89,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#torusKnot").show();
                 $("#obj").hide();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
 
             $("#btnParametric").click( (function() {
@@ -95,6 +101,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").show();
                 $("#obj").hide();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
             
             $("#btnObj").click( (function() {
@@ -106,6 +113,7 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").hide();
                 $("#obj").show();
                 $("#robot").hide();
+                $("#tangent").hide();
             }));
 
             $("#btnRobot").click((function(){
@@ -117,6 +125,19 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 $("#parametricSurface").hide();
                 $("#obj").hide();
                 $("#robot").show();
+                $("#tangent").hide();
+            }));
+
+            $("#btnTangent").click((function(){
+                $("#random").hide();
+                $("#band").hide();
+                $("#box").hide();
+                $("#sphere").hide();
+                $("#torusKnot").hide();
+                $("#parametricSurface").hide();
+                $("#obj").hide();
+                $("#robot").hide();
+                $("#tangent").show();
             }));
 
             $("#btnNewRandom").click( (function() {
@@ -238,6 +259,16 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
                 scene.addMesh(robot.getMesh());
             }));
 
+            $("#btnCalcTangent").click((function(){
+                var centerInput = $("#tCenter").attr("value").split(",");
+                var center = [parseFloat(centerInput[0]), parseFloat(centerInput[1])];
+                var radius = $("#tRadius").attr("value");
+                var pointInput = $("#tPoint").attr("value").split(",");
+                var point = [parseFloat(pointInput[0]), parseFloat(pointInput[1])];
+
+                console.log(tangentPoints(radius, center, point));
+            }));
+
             
             var paraFSet = function(id, func, umin = 0, umax = 10, vmin = 0, vmax = 10, scal = 50){
                 $(id).click(function(){
@@ -252,6 +283,54 @@ define(["jquery", "BufferGeometry", "random", "band", "parametric", "objmesh", "
             paraFSet("#fForSphere", "[Math.sin(u)*Math.cos(v), Math.sin(v), Math.cos(u)*Math.cos(v)]", 0, Math.PI*2, 0, Math.PI*2, 400);
             paraFSet("#fForFlower", "[(u-(u*u*u/3+u*v*v)), (v-(v*v*v/3+u*u*v)), (u*u-v*v)]", -2, 2, -2, 2, 100);
             paraFSet("#fForApple", "[Math.cos(u)*(4 + 3.8 * Math.cos(v)), Math.sin(u)*(4 + 3.8 * Math.cos(v)), ((Math.cos(v)+Math.sin(v)-1) * (1+Math.sin(v)) * Math.log(1-Math.PI * v/10)+7.5*Math.sin(v))]", 0, 2*Math.PI, -Math.PI, Math.PI, 50);
+
+
+            var tangentPoints = function(radius, o, p){
+
+                // vector between O and P
+                var op = [p[0]-o[0], p[1]-o[1]];
+                var lengthOP = Math.sqrt(op[0]*op[0] + op[1]*op[1]);
+
+                // angle between OP and radius side
+                var cosA = radius / lengthOP;
+
+                // length of the side between Q and T1 / T2
+                // where Q is the intersection point of T1T2 with OP
+                var sinA = Math.sqrt(1-cosA*cosA);
+
+                var lengthQT = sinA * radius;
+
+                // length of the side between O and Q
+                var lengthOQ = Math.sqrt(radius*radius - lengthQT*lengthQT);
+
+                // finding the coordinates of the point Q
+                var d = lengthOQ * Math.sqrt(p[0]*p[0]+p[1]*p[1])/(p[0]*p[0]+p[1]*p[1]);
+                var q = [o[0] + d*op[0], o[1] + d*op[1]];
+
+                // finding orthogonal vectors to OQ
+                var oq = [q[0] - o[0], q[1] - o[1]];
+
+                var a,b;
+                if(oq[0]===0){
+                    a = lengthQT;
+                    b = 0;
+                } else if(oq[1]===0){
+                    b = lengthQT;
+                    a = 0;
+                } else {
+                    a = lengthQT * Math.sqrt(1+(oq[0]*oq[0]/oq[1]*oq[1]));
+                    b = -a*oq[1]/oq[0];
+                }
+
+                var u1 = [a, b];
+                var u2 = [-a, -b];
+
+                // finding the coordinates of the tangent points
+                var t1 = [o[0]+oq[0]+u1[0], o[1]+oq[1]+u1[1]];
+                var t2 = [o[0]+oq[0]+u2[0], o[1]+oq[1]+u2[1]];
+
+                return [t1, t2];
+            };
 
         };
 
